3ffab540235368f53d43be950db76ccd7354515e
fahad39 <fahadhussain0127@gmail.com>
2023-12-19 19:53:36 +0500

    added new nav bar styles

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import classNames from "classnames";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
@@ -19,9 +20,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className={`${
-              link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
-            } hover:text-zinc-800 transition-colors`}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
