35120249704c5753d912b9401bbd5c3f3f11bbb3
fahad39 <fahadhussain0127@gmail.com>
2023-12-19 17:19:12 +0500

    added react icons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import React from "react";
+import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
   return (
     <nav className="flex space-x-6 mb-2 border-b p-10">
-      <Link href={"/"}>Logo</Link>
+      <Link href={"/"}>
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
         <li>
           <Link href={"/"}>Dashboard</Link>
